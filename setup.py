f894cf1e79a928ae545d2e100e6902519b1d17f5
Mitch Garnaat <mitch@garnaat.com>
2016-01-15 08:49:49 -0500

    bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='placebo',
-    version='0.4.3',
+    version='0.5.0',
     description='Make boto3 calls that look real but have no effect',
     long_description=open('README.md').read(),
     author='Mitch Garnaat',
