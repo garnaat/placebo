4c79a52cf76b84790ed6c65ae6a3136884e77c06
Vasco Pinho <vascogpinho@gmail.com>
2016-02-29 00:54:38 +0000

    fix decorator test

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -7,6 +7,7 @@ import mock
 
 from placebo.utils import placebo_session
 
+
 class TestUtils(unittest.TestCase):
 
     def setUp(self):
@@ -17,6 +18,7 @@ class TestUtils(unittest.TestCase):
                                        'aws_credentials')
         self.environ['AWS_SHARED_CREDENTIALS_FILE'] = credential_path
         self.environ['PLACEBO_MODE'] = 'record'
+        self.environ['PLACEBO_DIR'] = 'placebo_test_runs'
         self.session = boto3.Session(profile_name='foobar',
                                      region_name='us-west-2')
 
@@ -24,7 +26,7 @@ class TestUtils(unittest.TestCase):
     def test_decorator(self, session):
 
         # Tear it up..
-        PLACEBO_TEST_DIR = os.path.join(os.getcwd(),'placebo', 'placebo')
+        PLACEBO_TEST_DIR = os.path.join(os.getcwd(), 'placebo_test_runs')
         prefix = 'TestUtils.test_decorator'
         record_dir = os.path.join(PLACEBO_TEST_DIR, prefix)
         self.assertTrue(os.path.exists(record_dir))
